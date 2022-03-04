fc352fcc4b932d901002fa59c3e7be3bbd30cb86
quanshi2 <quanshi@hotmail.com>
2022-03-03 22:23:54 -0800

    Add files via upload

---
+++ myWebApp_main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  3 22:03:34 2022
+
+@author: Yun An Shi
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+
