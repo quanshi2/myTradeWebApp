206fbc6d707eb4a544d777aa6ec6b0ef6b7f4b6c
quanshi2 <quanshi@hotmail.com>
2022-03-05 19:02:22 -0800

    the first test
    a simple test

--- myWebApp_main.py
+++ myWebApp_main.py
@@ -11,3 +11,21 @@ import numpy as np
 
 st.title('Uber pickups in NYC')
 
+# Create a table
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+# draw chart
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+# Widgets， slider
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
